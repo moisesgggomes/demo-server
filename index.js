222981d92fe36b94c7dbbc1b53cb71a0458c510a
Moises Gomes <moisesgomes376@gmail.com>
2022-06-09 13:31:37 -0300

    Modify server

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("pages/index");
+    res.sendFile(path.join(__dirname, "views/pages/index.html"));
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on ${port} http://localhost:${port}`));
